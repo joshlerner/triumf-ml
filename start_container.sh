e355fdb38b6ef9b1cae0a2983f769275934e7a76
Joshua <joshlerner03@gmail.com>
2023-05-15 11:42:17 -0700

    untracked changes

--- start_container.sh
+++
@@ -1,2 +0,0 @@
-dig +short myip.opendns.com @resolver1.opendns.com > ip.txt
-singularity exec --nv -B /fast_scratch_1 -B /fast_scratch_2 -B /data /fast_scratch_1/triumfmlutils/containers/container_base_ml_tf_v0.1.38.sif  /bin/bash
